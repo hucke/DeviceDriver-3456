fe256ea22f46bed07bc57044ca07ad9c2a980d4c
Kim, dong su <hucke@hanmail.net>
2023-06-05 15:49:26 +0900

    add unittest codes

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,32 @@
 #include "DeviceDriver.h"
+#include <stdexcept>
 
-DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
+class ReadFailException : public std::exception
+{
+public:
+};
+
+DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+
+    int result = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+        if (i == 0) {
+            result = (int)(m_hardware->read(address));
+            continue;
+        }
+
+        int value = (int)(m_hardware->read(address));
+        if (result != value)
+            throw ReadFailException();
+    }
+
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,43 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "../DeviceDriver/DeviceDriver.cpp"
+
+using namespace testing;
+
+class FlasMemoryDeviceMock : public FlashMemoryDevice
+{
+public:
+    // unsigned char read(long address) = 0;
+    // void write(long address, unsigned char data) = 0;
+    MOCK_METHOD(unsigned char, read, (long), (override));
+    MOCK_METHOD(void, write, (long, unsigned char), (override));
+};
+
+// 대역
+// 스터빙 or Behavior 검증
+// 
+
+TEST(DeviceDriver, ReadNormal)
+{
+    FlasMemoryDeviceMock fmdMock;
+    EXPECT_CALL(fmdMock, read)
+        .Times(5)
+        .WillRepeatedly(Return(5));
+
+
+    DeviceDriver dd(&fmdMock);
+    EXPECT_THAT(dd.read(0), Eq(5));
+}
+
+TEST(DeviceDriver, ReadFail)
+{
+    FlasMemoryDeviceMock fmdMock;
+    EXPECT_CALL(fmdMock, read(0x0A))
+        .WillOnce(Return(1))
+        .WillRepeatedly(Return(5));
+
+
+    DeviceDriver dd(&fmdMock);
+    EXPECT_THROW(dd.read(0x0A), ReadFailException);
 }
